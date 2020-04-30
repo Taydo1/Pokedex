de084a8fae836f2995700daf3d5683f9aefb577d
Taydo <leon.marteau.lm@gmail.com>
2020-04-30 15:07:21 +0200

    AJout creation bdd

--- src/pokedex/Pokedex.java
+++ src/pokedex/Pokedex.java
@@ -5,7 +5,7 @@
  */
 package pokedex;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.sql.DriverManager;
 
 /**
@@ -19,10 +19,24 @@ public class Pokedex {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Connection c = null;
+        String dbName="pokedex";
+        String schemaName ="pokedex";
+        String[] tables = {"test1"," test2"};
+        Statement st;
         try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "hugoquentinleon");
+            st = connectDB("");
+            st.executeUpdate("DROP database IF EXISTS "+dbName+";");
+            st.executeUpdate("CREATE database "+dbName+";");
+            st = connectDB(dbName);
+            st.executeUpdate("CREATE SCHEMA "+schemaName+";");
+            st.executeUpdate("SET search_path TO"+schemaName);
+            st.executeUpdate("CREATE TABLE pokedex("
+                +"id serial PRIMARY KEY"
+                +"nom varchar(30));");
+            
+            // vvv POur lister toutes les tables/schema de la bdd vvv
+            //SELECT table_schema, table_name FROM information_schema.tables WHERE table_schema NOT IN ('pg_catalog', 'information_schema');
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
@@ -30,5 +44,9 @@ public class Pokedex {
         }
         System.out.println("Opened database successfully NOOOOOOO");
     }
-
+    
+    public static Statement connectDB(String dbName) throws SQLException {
+        Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+dbName, "postgres", "hugoquentinleon");
+        return c.createStatement();
+    }
 } 

257adafe04bc3561f9eab66ffef529b5f97cfd88
Spectan <Spectan@MSI>
2020-05-07 22:57:04 +0200

    C'est encore plus mieux maintenant

--- src/pokedex/SelectionPanel.java
+++ src/pokedex/SelectionPanel.java
@@ -8,6 +8,8 @@ package pokedex;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -15,6 +17,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 /**
  *
@@ -25,7 +28,7 @@ public class SelectionPanel extends JPanel{
     JFrame frame;
     JPanel gauche, droite, allerId;
     JLabel idActuel, nom, allerAId;
-    JButton up, down, go, modification, ajout;
+    JButton up, down, go, modification, ajout, gerer;
     JTextField goId;
     
     public SelectionPanel(int id, String name, JFrame f, String utilisateur){
@@ -34,9 +37,9 @@ public class SelectionPanel extends JPanel{
         gauche = new JPanel();
         droite = new JPanel();
         allerId = new JPanel();
-        idActuel = new JLabel("ID actuel : " + String.valueOf(id));
-        nom = new JLabel("Nom : " + name);
-        allerAId = new JLabel("aller à l'ID : ");
+        idActuel = new JLabel("ID actuel : " + String.valueOf(id),SwingConstants.CENTER);
+        nom = new JLabel("Nom : " + name,SwingConstants.CENTER);
+        allerAId = new JLabel("Aller à l'ID : ",SwingConstants.CENTER);
         up = new JButton(new ImageIcon(getClass().getResource("/images/icones/fleche_haut.png")));
         up.setContentAreaFilled(false);
         up.setCursor(Cursor.getPredefinedCursor((Cursor.HAND_CURSOR)));
@@ -53,7 +56,11 @@ public class SelectionPanel extends JPanel{
         ajout = new JButton("Ajouter un pokémon");
         ajout.setBackground(Color.gray);
         ajout.setCursor(Cursor.getPredefinedCursor((Cursor.HAND_CURSOR)));
-        ajout.setForeground(Color.black); 
+        ajout.setForeground(Color.black);
+        gerer = new JButton("Gérer l'équipe");
+        gerer.setBackground(Color.gray);
+        gerer.setCursor(Cursor.getPredefinedCursor((Cursor.HAND_CURSOR)));
+        gerer.setForeground(Color.black);
         goId = new JTextField();
         goId.setBackground(Color.gray);
         goId.setForeground(Color.white);
@@ -63,41 +70,58 @@ public class SelectionPanel extends JPanel{
         gauche.setBackground(Color.gray);
         droite.setLayout(new BorderLayout());
         droite.setBackground(Color.gray);
-        allerId.setLayout(new BorderLayout());
+        allerId.setLayout(new GridBagLayout());
         allerId.setBackground(Color.gray);
-        setLayout(new GridLayout(0,2));
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
         
-        allerId.add(this.allerAId, BorderLayout.WEST);
-        allerId.add(this.goId, BorderLayout.EAST);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 0.4;
+        allerId.add(allerAId, c);
+        c.gridx = 1;
+        c.weightx = 0.2;
+        allerId.add(goId, c);
+        c.gridx = 2;
+        c.weightx = 0.4;
+        allerId.add(go, c);
         
-        gauche.add(idActuel);
         gauche.add(nom);
         gauche.add(allerId);
         gauche.add(modification);
         gauche.add(ajout);
-        gauche.setSize(frame.getWidth()/2, frame.getHeight()/2);
+        gauche.add(gerer);
+        gauche.setSize((int)(frame.getWidth()*0.7), (int)(frame.getHeight()*0.2));
         
         droite.add(up, BorderLayout.NORTH);
-        droite.add(go, BorderLayout.CENTER);
+        droite.add(idActuel, BorderLayout.CENTER);
         droite.add(down, BorderLayout.SOUTH);
-        droite.setSize(frame.getWidth()/2, frame.getHeight()/2);
+        droite.setSize((int)(frame.getWidth()*0.3), (int)(frame.getHeight()*0.2));
         
         switch (utilisateur){
             case "professeur" :
                 ajout.setEnabled(true);
-                modification.setEnabled(true);                
+                modification.setEnabled(true);  
+                gerer.setEnabled(false);              
                 break;
             case "dresseur" :
                 ajout.setEnabled(true);
-                modification.setEnabled(false);                
+                modification.setEnabled(false);  
+                gerer.setEnabled(true);              
                 break;
             case "visiteur" :
                 ajout.setEnabled(false);
                 modification.setEnabled(false);
+                gerer.setEnabled(false);
                 break;
         }
-        
-        add(gauche);
-        add(droite);
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.weightx = 0.7;
+        add(gauche , c);
+        c.gridx = 1;
+        c.weightx = 0.3;
+        add(droite, c);
     }
 }

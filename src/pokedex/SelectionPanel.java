6461b748c4ab84abd3fcdf1d36a7630a71d7c7df
Spectan <Spectan@MSI>
2020-05-07 21:11:55 +0200

    Panneau du bas (à arranger)

---
+++ src/pokedex/SelectionPanel.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokedex;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.GridLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Spectan
+ */
+public class SelectionPanel extends JPanel{
+    
+    JFrame frame;
+    JPanel gauche, droite, allerId;
+    JLabel idActuel, nom, allerAId;
+    JButton up, down, go;
+    JTextField goId;
+    
+    public SelectionPanel(int id, String name, JFrame f){
+        
+        frame = f;
+        gauche = new JPanel();
+        droite = new JPanel();
+        allerId = new JPanel();
+        idActuel = new JLabel("ID actuel : " + String.valueOf(id));
+        nom = new JLabel("Nom : " + name);
+        allerAId = new JLabel("aller à l'ID : ");
+        up = new JButton(new ImageIcon(getClass().getResource("/images/icones/fleche_haut.png")));
+        up.setContentAreaFilled(false);
+        up.setCursor(Cursor.getPredefinedCursor((Cursor.HAND_CURSOR)));
+        down = new JButton(new ImageIcon(getClass().getResource("/images/icones/fleche_bas.png")));
+        down.setContentAreaFilled(false);
+        down.setCursor(Cursor.getPredefinedCursor((Cursor.HAND_CURSOR)));
+        go = new JButton("GO to ID");
+        go.setBackground(Color.gray);
+        go.setCursor(Cursor.getPredefinedCursor((Cursor.HAND_CURSOR)));
+        goId = new JTextField();
+        goId.setBackground(Color.gray);
+        goId.setForeground(Color.white);
+        goId.setColumns(3);
+        
+        gauche.setLayout(new GridLayout(0, 1));
+        gauche.setBackground(Color.gray);
+        droite.setLayout(new BorderLayout());
+        droite.setBackground(Color.gray);
+        allerId.setLayout(new BorderLayout());
+        allerId.setBackground(Color.gray);
+        setLayout(new GridLayout(0,2));
+        
+        allerId.add(this.allerAId, BorderLayout.WEST);
+        allerId.add(this.goId, BorderLayout.EAST);
+        
+        gauche.add(idActuel);
+        gauche.add(nom);
+        gauche.add(allerId);
+        gauche.setSize(frame.getWidth()/2, frame.getHeight()/2);
+        
+        droite.add(up, BorderLayout.NORTH);
+        droite.add(go, BorderLayout.CENTER);
+        droite.add(down, BorderLayout.SOUTH);
+        droite.setSize(frame.getWidth()/2, frame.getHeight()/2);
+        
+        add(gauche);
+        add(droite);
+    }
+}

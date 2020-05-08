47a87922e1d5be6b2d99fb11255187ef6633bce2
Taydo <leon.marteau.lm@gmail.com>
2020-05-08 11:07:52 +0200

    correction nom variables incorrecte

--- src/pokedex/Pokedex.java
+++ src/pokedex/Pokedex.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
  */
 public class Pokedex extends DBElement {
 
-    String name, en_name, classfication;
+    String name, en_name, classification;
     int id, id_type1, id_type2, id_ability1, id_ability2, id_ability3, id_ability4,
             generation, id_lower_evolution, id_evolution;
     float height, weight, percentage_male;
@@ -25,11 +25,11 @@ public class Pokedex extends DBElement {
 
     public Pokedex() {}
     
-    public Pokedex(String name, String en_name, String classfication, int id_type1, int id_type2, int id_ability1, int id_ability2, int id_ability3, int id_ability4, int generation, int id_lower_evolution, int id_evolution, float height, float weight, float percentage_male, boolean is_legendary) {
+    public Pokedex(String name, String en_name, String classification, int id_type1, int id_type2, int id_ability1, int id_ability2, int id_ability3, int id_ability4, int generation, int id_lower_evolution, int id_evolution, float height, float weight, float percentage_male, boolean is_legendary) {
         this.id = -1;
         this.name = name;
         this.en_name = en_name;
-        this.classfication = classfication;
+        this.classification = classification;
         this.id_type1 = id_type1;
         this.id_type2 = id_type2;
         this.id_ability1 = id_ability1;
@@ -51,7 +51,7 @@ public class Pokedex extends DBElement {
         this.id = -1;
         this.name = infos[1];
         this.en_name = infos[2];
-        this.classfication = infos[3];
+        this.classification = infos[3];
         this.id_type1 = type2id.get(infos[4]);
         this.id_type2 = type2id.get(infos[5]);
         this.id_ability1 = ability2id.get(infos[6]);
@@ -71,7 +71,7 @@ public class Pokedex extends DBElement {
         this.id = rs.getInt("id");
         this.name = rs.getString("name");
         this.en_name = rs.getString("en_name");
-        this.classfication = rs.getString("classfication");
+        this.classification = rs.getString("category");
         this.id_ability1 = rs.getInt("id_ability1");
         this.id_ability2 = rs.getInt("id_ability2");
         this.id_ability3 = rs.getInt("id_ability3");
@@ -90,14 +90,14 @@ public class Pokedex extends DBElement {
 
     @Override
     public String toString() {
-        return "Pokedex{" + "name=" + name + ", en_name=" + en_name + ", classfication=" + classfication + ", id=" + id + ", id_type1=" + id_type1 + ", id_type2=" + id_type2 + ", id_ability1=" + id_ability1 + ", id_ability2=" + id_ability2 + ", id_ability3=" + id_ability3 + ", id_ability4=" + id_ability4 + ", generation=" + generation + ", id_lower_evolution=" + id_lower_evolution + ", id_evolution=" + id_evolution + ", height=" + height + ", weight=" + weight + ", percentage_male=" + percentage_male + ", is_legendary=" + is_legendary + '}';
+        return "Pokedex{" + "name=" + name + ", en_name=" + en_name + ", classification=" + classification + ", id=" + id + ", id_type1=" + id_type1 + ", id_type2=" + id_type2 + ", id_ability1=" + id_ability1 + ", id_ability2=" + id_ability2 + ", id_ability3=" + id_ability3 + ", id_ability4=" + id_ability4 + ", generation=" + generation + ", id_lower_evolution=" + id_lower_evolution + ", id_evolution=" + id_evolution + ", height=" + height + ", weight=" + weight + ", percentage_male=" + percentage_male + ", is_legendary=" + is_legendary + '}';
     }
     
     @Override
     public String getInsertSubRequest() {
         return String.format(Locale.ROOT, "(default, '%s', '%s', '%s', %d, %s, %s, %s, %s, %s, %f, %f, %s, %b, %d,%s, %s)",
                 name.replace("'", "''"), en_name.replace("'", "''"),
-                classfication.replace("'", "''"),
+                classification.replace("'", "''"),
                 id_type1, int2StringRequest(id_type2),
                 id_ability1, int2StringRequest(id_ability2), int2StringRequest(id_ability3), int2StringRequest(id_ability4),
                 height, weight, float2StringRequest(percentage_male), is_legendary, generation,

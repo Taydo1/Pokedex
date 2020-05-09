a8762660247c96931d43dea991ba0dc48ee23069
Taydo <leon.marteau.lm@gmail.com>
2020-05-09 11:42:12 +0200

    suppression include inutiles

--- src/pokedex/TopPokedexPanel.java
+++ src/pokedex/TopPokedexPanel.java
@@ -10,7 +10,6 @@ import java.awt.Cursor;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import javax.swing.ButtonGroup;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
